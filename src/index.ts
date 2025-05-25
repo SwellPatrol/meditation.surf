6323210c39186ab6d9f15f4a406af610c17d89cb
Garrett Brown <eigendebugger@gmail.com>
2025-05-24 23:26:58 -0700

    Import web tooling and initial framework
    Language:
    
      * Typescript
    
    Packaging:
    
      * pnpm
    
    Static analysis:
    
      * audit-ci
      * eslint
      * prettier
    
    Tooling:
    
      * Vite
    
    Framework:
    
      * LightningJS

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2025 Garrett Brown
+ * This file is part of meditation.surf - https://github.com/eigendude/meditation.surf
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ * See the file LICENSE.txt for more information.
+ */
+
+import { launchLightningApp } from "./LightningApp";
+
+/**
+ * Application entry point. Launches the LightningJS view.
+ */
+launchLightningApp();
