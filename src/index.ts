37eb2972e69c5010c1ccba89646d018655122874
Garrett Brown <eigendebugger@gmail.com>
2025-05-24 23:26:58 -0700

    Import new Lightningjs site and packaging

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2025 Garrett Brown
+ * This file is part of meditation.surf - https://github.com/eigendude/meditation.surf
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ * See the file LICENSE.txt for more information.
+ */
+
+import Blits from "@lightningjs/blits";
+
+import App from "./App";
+
+Blits.Launch(App, "app", {
+  w: 3840,
+  h: 2160,
+  debugLevel: 1,
+  fonts: [
+    {
+      family: "lato",
+      type: "msdf",
+      file: "fonts/Lato-Regular.ttf",
+    },
+    {
+      family: "raleway",
+      type: "msdf",
+      file: "fonts/Raleway-ExtraBold.ttf",
+    },
+    {
+      family: "opensans",
+      type: "web",
+      file: "fonts/OpenSans-Medium.ttf",
+    },
+  ],
+});
