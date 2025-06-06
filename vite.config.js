29c900edd4a5f57e97e9cefc7bd84cb60a6b2fa6
Garrett Brown <garrett.brown@aclima.io>
2025-06-06 09:14:20 -0700

    Remove unused options from Vite config

--- vite.config.js
+++ vite.config.js
@@ -9,25 +9,13 @@
 import blitsVitePlugins from "@lightningjs/blits/vite";
 import { defineConfig } from "vite";
 
-// eslint-disable-next-line no-unused-vars
-export default defineConfig(({ command, mode, ssrBuild }) => {
-  return {
-    base: "/", // Set to your base path if you are deploying to a subdirectory (example: /myApp/)
-    plugins: [...blitsVitePlugins],
-    resolve: {
-      mainFields: ["browser", "module", "jsnext:main", "jsnext"],
+export default defineConfig({
+  base: "/", // Set to your base path if you are deploying to a subdirectory (example: /myApp/)
+  plugins: [...blitsVitePlugins],
+  server: {
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Embedder-Policy": "require-corp",
     },
-    server: {
-      headers: {
-        "Cross-Origin-Opener-Policy": "same-origin",
-        "Cross-Origin-Embedder-Policy": "require-corp",
-      },
-      fs: {
-        allow: [".."],
-      },
-    },
-    worker: {
-      format: "es",
-    },
-  };
+  },
 });
