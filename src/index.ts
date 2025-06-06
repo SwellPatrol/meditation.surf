bf028ee15017ec517da4d8debaab72b5b04c5e87
Garrett Brown <garrett.brown@aclima.io>
2025-06-05 17:20:40 -0700

    remove unused loader

--- src/index.ts
+++ src/index.ts
@@ -13,22 +13,4 @@ import App from "./App";
 Blits.Launch(App, "app", {
   w: window.innerWidth,
   h: window.innerHeight,
-  debugLevel: 1,
-  fonts: [
-    {
-      family: "lato",
-      type: "msdf",
-      file: "fonts/Lato-Regular.ttf",
-    },
-    {
-      family: "raleway",
-      type: "msdf",
-      file: "fonts/Raleway-ExtraBold.ttf",
-    },
-    {
-      family: "opensans",
-      type: "web",
-      file: "fonts/OpenSans-Medium.ttf",
-    },
-  ],
 });
