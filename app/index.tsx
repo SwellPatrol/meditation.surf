0e2e7226c36ae6d3974ea45985a06b4c812b024b
Garrett Brown <garrett.brown@aclima.io>
2025-06-23 17:02:11 -0700

    Add fullscreen video background

--- app/index.tsx
+++ app/index.tsx
@@ -6,13 +6,27 @@
  * See the file LICENSE.txt for more information.
  */
 
-import React from "react";
+import { ResizeMode, Video } from "expo-av";
+import React, { useRef } from "react";
 import { Image, StyleSheet, View } from "react-native";
 
 export default function HomeScreen(): JSX.Element {
+  const videoRef: React.RefObject<Video> = useRef<Video>(null);
+
   return (
     <View style={styles.container}>
       <Image source={require("@/assets/images/icon.png")} style={styles.icon} />
+      <Video
+        ref={videoRef}
+        style={styles.video}
+        source={{
+          uri: "https://stream.mux.com/7YtWnCpXIt014uMcBK65ZjGfnScdcAneU9TjM9nGAJhk.m3u8",
+        }}
+        resizeMode={ResizeMode.COVER}
+        shouldPlay
+        isLooping
+        useNativeControls={false}
+      />
     </View>
   );
 }
@@ -20,12 +34,15 @@ export default function HomeScreen(): JSX.Element {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#000000",
     justifyContent: "center",
     alignItems: "center",
   },
   icon: {
+    position: "absolute",
     width: "100%",
     height: "100%",
     resizeMode: "contain",
   },
+  video: StyleSheet.absoluteFillObject,
 });
